e9412de8481656d09452d8864e283fdf7540b9a7
Ornni <106477266+ornni@users.noreply.github.com>
2024-04-01 10:06:17 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.38 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    
+    ap = [0] * (n+2)
+    ap[1] = 1
+    ap[2] = 2
+    
+    if n <= 2:
+        return ap[n] % 1234567
+    else:
+        for i in range(3, n+1):
+            ap[i] = ap[i-1] + ap[i-2]
+        return ap[n] % 1234567
+        
