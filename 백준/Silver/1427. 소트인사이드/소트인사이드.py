d32728dad3854ba7d7680197a6efd53198f7fdb9
Ornni <106477266+ornni@users.noreply.github.com>
2024-04-04 09:35:25 +0900

    [Silver V] Title: 소트인사이드, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -2,14 +2,13 @@ import sys
 input = sys.stdin.readline
 
 A = list(input())
-
+A = A[:-1]
 n = len(A)
 for i in range(n):
-    for j in range(n-i-1):
-        if A[j] > A[j+1]:
+    for j in range(n-1, i, -1):
+        if A[j] > A[j-1]:
             temp = A[j]
-            A[j] = A[j+1]
-            A[j+1] = temp
-A.reverse()
+            A[j] = A[j-1]
+            A[j-1] = temp
 
 print(''.join(A))
