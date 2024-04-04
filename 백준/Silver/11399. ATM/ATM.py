e42621f30b466e8f24fe516478f158678cadf66f
Ornni <106477266+ornni@users.noreply.github.com>
2024-04-04 14:01:26 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -4,16 +4,7 @@ input = sys.stdin.readline
 n = int(input())
 A = list(map(int, input().split()))
 
-for i in range(n):
-    target_data = A[i]
-    left_interval = A[i+1:]
-    
-    for j in range(i):
-        if target_data < A[j]:
-            interval = A[j:i]
-            A = A[:j] + [target_data] + interval + left_interval
-            break
-
+A.sort()
 
 cum = []
 cum_num = 0
