e6e7868abc7bcf3f23f642779265cc530a16b1ad
Ornni <106477266+ornni@users.noreply.github.com>
2024-04-19 08:44:55 +0900

    [level 1] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,27 @@
+def solution(cards1, cards2, goal):
+    answer = ""
+    cards1.append(" ")
+    cards2.append(" ")
+
+    index1 = 0
+    index2 = 0
+    A = []
+
+    for i in goal:
+        if i == cards1[index1]:
+            A.append(cards1[index1])
+            if index1 < (len(cards1) - 1):
+                index1 += 1
+
+        elif i == cards2[index2]:
+            A.append(cards2[index2])
+            if index2 < (len(cards2) - 1):
+                index2 += 1
+        else:
+            break
+
+    if A == goal:
+        answer += "Yes"
+    else:
+        answer += "No"
+    return answer
