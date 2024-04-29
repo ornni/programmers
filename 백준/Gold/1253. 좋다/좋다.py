8d8c5f4327df397b459c432bd3f21bf239cd2767
Ornni <106477266+ornni@users.noreply.github.com>
2024-04-29 11:34:27 +0900

    [Gold IV] Title: 좋다, Time: 1024 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -2,27 +2,28 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-number = list(map(int, input().split()))
-answer = 0
-sort_number = sorted(number)
+A = list(map(int, input().split()))
 
-for k in range(n):
-    target = sort_number[k]
-    one = 0
-    two = n-1
-    while one < two:
-        sum = sort_number[one] + sort_number[two]
-        if sum == target:
-            if one != k and two != k:
+answer = 0
+A.sort()
+
+for i in range(n):
+    find = A[i]
+    index1 = 0
+    index2 = n - 1
+    while index1 < index2:
+        total = A[index1] + A[index2]
+        if total == find:
+            if index1 != i and index2 != i:
                 answer += 1
                 break
-            elif one == k:
-                one += 1
-            elif two == k:
-                two -= 1
-        elif sum < target:
-            one += 1
+            elif index1 == i:
+                index1 += 1
+            elif index2 == i:
+                index2 -= 1
+        elif total < find:
+            index1 += 1
         else:
-            two -= 1
+            index2 -= 1
 
 print(answer)
