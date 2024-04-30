650c73b5df6bbad75a44ae847a4705f462fb25cb
Ornni <106477266+ornni@users.noreply.github.com>
2024-04-30 11:21:48 +0900

    [Silver II] Title: DNA 비밀번호, Time: 468 ms, Memory: 39804 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.py
@@ -1,78 +1,66 @@
 import sys
 input = sys.stdin.readline
 
-need_list = [0]*4
-now_list = [0]*4
-check = 0
-
-def add_in_list(x):
-    global need_list, now_list, check
-    
-    if x == 'A':
-        now_list[0] += 1
-        if now_list[0] == need_list[0]:
-            check += 1
-    
-    elif x == 'C':
-        now_list[1] += 1
-        if now_list[1] == need_list[1]:
-            check += 1
-    
-    elif x == 'G':
-        now_list[2] += 1
-        if now_list[2] == need_list[2]:
-            check += 1
-    
-    else:
-        now_list[3] += 1
-        if now_list[3] == need_list[3]:
-            check += 1
-
-def remove_in_list(x):
-    global need_list, now_list, check
-    
-    if x == 'A':
-        if now_list[0] == need_list[0]:
-            check -= 1
-        now_list[0] -= 1
-    
-    elif x == 'C':
-        if now_list[1] == need_list[1]:
-            check -= 1
-        now_list[1] -= 1
-        
-    elif x == 'G':
-        if now_list[2] == need_list[2]:
-            check -= 1
-        now_list[2] -= 1
-    
-    else:
-        if now_list[3] == need_list[3]:
-            check -= 1
-        now_list[3] -= 1
-
 s, p = map(int, input().split())
-A = list(input())
-result = 0
-need_list = list(map(int, input().split()))
+DNA = list(input())
+check_list = [0] * 4
+check = 0
+answer_list = list(map(int, input().split()))
+answer = 0
 
+def plus(x):
+    global check_list, check, answer_list
+    if x == 'A': 
+        check_list[0] += 1
+        if check_list[0] == answer_list[0]:
+            check += 1
+    elif x == 'C':
+        check_list[1] += 1
+        if check_list[1] == answer_list[1]:
+            check += 1
+    elif x == 'G':
+        check_list[2] += 1
+        if check_list[2] == answer_list[2]:
+            check += 1
+    elif x == 'T':
+        check_list[3] += 1
+        if check_list[3] == answer_list[3]:
+            check += 1
+
+def minus(x):
+    global check_list, check, answer_list
+    if x == 'A':
+        if check_list[0] == answer_list[0]:
+            check -= 1
+        check_list[0] -= 1
+    elif x == 'C':
+        if check_list[1] == answer_list[1]:
+            check -= 1
+        check_list[1] -= 1
+    elif x == 'G':
+        if check_list[2] == answer_list[2]:
+            check -= 1
+        check_list[2] -= 1
+    elif x == 'T':
+        if check_list[3] == answer_list[3]:
+            check -= 1
+        check_list[3] -= 1
+            
 for i in range(4):
-    if need_list[i] == 0:
+    if answer_list[i] == 0:
         check += 1
 
 for i in range(p):
-    add_in_list(A[i])
+    plus(DNA[i])
     
 if check == 4:
-    result += 1
+    answer += 1
 
 for i in range(p, s):
     j = i - p
-    add_in_list(A[i])
-    remove_in_list(A[j])
-    
+    plus(DNA[i])
+    minus(DNA[j])
     if check == 4:
-        result += 1
-
-print(result)
-    
+        answer += 1
+        
+print(answer)
