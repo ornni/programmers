621670174220f815bf706ff9ffaf0482210598c8
Ornni <106477266+ornni@users.noreply.github.com>
2024-05-02 10:27:54 +0900

    [Gold V] Title: 신기한 소수, Time: 7124 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -1,24 +1,24 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10000)
 
 n = int(input())
 
-def prime(x):
-    for i in range(2, int((x/2)+1)):
+def isPrime(x):
+    for i in range(2, int(x/2)+1):
         if x % i == 0:
             return False
     return True
 
-def DFS(num):
-    if len(str(num)) == n:
-        print(num)
+def DFS(x):
+    if len(str(x)) == n:
+        print(x)
     else:
-        for i in range(1, 10):
-            if i % 2 == 0:
-                continue
-            if prime(num * 10 + i):
-                DFS(num * 10 + i)
+        for i in range(1, 10, 2):
+            if isPrime(x * 10 + i):
+                DFS(x * 10 + i)
+
 DFS(2)
 DFS(3)
 DFS(5)
-DFS(7) 
+DFS(7)
