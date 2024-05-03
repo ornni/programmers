ff0e516e12c56b7606422ff091119237b86bb3c2
Ornni <106477266+ornni@users.noreply.github.com>
2024-05-03 09:52:11 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -5,10 +5,8 @@ def solution(k, score):
     for i in range(len(score)):
         score_in.append(score[i])
         score_in.sort(reverse = True)
-        
         if len(score_in) > k:
             del score_in[-1]
-            
         answer.append(score_in[-1])
     
     return answer
