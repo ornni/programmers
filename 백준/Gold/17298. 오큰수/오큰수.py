fc48f76ec76a676ebace077b50e110d7e8706886
Ornni <106477266+ornni@users.noreply.github.com>
2024-05-07 11:41:38 +0900

    [Gold IV] Title: 오큰수, Time: 1076 ms, Memory: 155536 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -3,7 +3,6 @@ input = sys.stdin.readline
 
 n = int(input())
 A = list(map(int, input().split()))
-
 stack = []
 answer = [0] * n
 
@@ -11,8 +10,8 @@ for i in range(n):
     while stack and A[stack[-1]] < A[i]:
         answer[stack.pop()] = A[i]
     stack.append(i)
-
+    
 while stack:
     answer[stack.pop()] = -1
-
+    
 print(*answer)
