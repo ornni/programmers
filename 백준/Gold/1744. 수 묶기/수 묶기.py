21da4c9bfe5e73d52c9b8b8e47cfcce3ceb7af58
Ornni <106477266+ornni@users.noreply.github.com>
2024-05-13 09:55:47 +0900

    [Gold IV] Title: 수 묶기, Time: 92 ms, Memory: 37012 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.py
+++ 백준/Gold/1744. 수 묶기/수 묶기.py
@@ -1,44 +1,44 @@
 import sys
-input = sys.stdin.readline
 from queue import PriorityQueue
+input = sys.stdin.readline
 
-n = int(input())
-plus_queue = PriorityQueue()
-minus_queue = PriorityQueue()
+plus = PriorityQueue()
+minus = PriorityQueue()
+
 one = 0
 zero = 0
-a = 0
-b= 0
 sum = 0
+
+n = int(input())
 
 for i in range(n):
     data = int(input())
     if data > 1:
-        plus_queue.put(data * (-1))
+        plus.put(data * (-1))
     elif data == 1:
         one += 1
     elif data == 0:
         zero += 1
     else:
-        minus_queue.put(data)
-        
+        minus.put(data)
 
-while plus_queue.qsize() > 1:
-    a = plus_queue.get() * (-1)
-    b = plus_queue.get() * (-1)
-    sum += a * b
+while plus.qsize() > 1:
+    first = plus.get() * (-1)
+    second = plus.get() * (-1)
+    sum += first * second
 
-if plus_queue.qsize() > 0:
-    sum += plus_queue.get() * (-1)
+if plus.qsize() > 0:
+    sum += plus.get() * (-1)
 
-while minus_queue.qsize() > 1:
-    a = minus_queue.get()
-    b = minus_queue.get()
-    sum += a * b
+while minus.qsize() > 1:
+    first = minus.get()
+    second = minus.get()
+    sum += first * second
 
-if minus_queue.qsize() > 0:
-    if zero == 0:
-        sum += minus_queue.get()
+if minus.qsize() > 0:
+    if zero == 0 :
+        sum += minus.get()
 
 sum += one
+
 print(sum)
