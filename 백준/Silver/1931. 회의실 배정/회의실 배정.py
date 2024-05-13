5ce0feb21ca5de4ba13d45ae729abd82e4f3cb0e
Ornni <106477266+ornni@users.noreply.github.com>
2024-05-13 09:59:49 +0900

    [Silver I] Title: 회의실 배정, Time: 276 ms, Memory: 46628 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,18 +1,21 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-A = [[] for _ in range(n)]
+A = [[0]*2 for _ in range(n)]
 
 for i in range(n):
     s, e = map(int, input().split())
-    A[i].append(e)
-    A[i].append(s)
-
+    A[i][0] = e
+    A[i][1] = s
+    
 A.sort()
+count = 0
 end = -1
-answer = 0
 
-for i in A:
-    if i[1] >= end:
-        answer += 1
-        end = i[0]
+for i in range(n):
+    if A[i][1] >= end:
+        end = A[i][0]
+        count += 1
 
-print(answer)
+print(count)
