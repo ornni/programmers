97134d1e742c1d8fd8ab9345c18c42168c20733c
Ornni <106477266+ornni@users.noreply.github.com>
2024-05-28 10:59:29 +0900

    [level 2] Title: 올바른 괄호, Time: 6.96 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,20 +1,7 @@
 def solution(s):
     answer = True
-    check = 0
     
-    for i in s:
-        if i == "(":
-            check += 1
-        elif i == ")":
-            check -= 1
-        
-        if check < 0:
-            answer = False
-            break
-    
-    if not answer:    
-        return False
-    elif check == 0:
-        return True
-    else:
-        return False
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    return True
