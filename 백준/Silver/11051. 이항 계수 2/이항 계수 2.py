3e6958c8a44503b8ad5587b83e2fe53bd0e4cbd1
Ornni <106477266+ornni@users.noreply.github.com>
2024-06-17 09:09:35 +0900

    [Silver II] Title: 이항 계수 2, Time: 308 ms, Memory: 54500 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+
+for i in range(0, n+1):
+    dp[i][1] = i
+    dp[i][0] = 1
+    dp[i][i] = 1
+
+for i in range(2, n+1):
+    for j in range(1, i):
+        dp[i][j] = dp[i-1][j] + dp[i-1][j-1]
+        dp[i][j] = dp[i][j] % 10007
+
+print(dp[n][k])
