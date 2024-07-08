1a4a81277d3b74a7478841dacd39c2d6ab61147d
Ornni <106477266+ornni@users.noreply.github.com>
2024-07-09 08:47:33 +0900

    [Gold V] Title: 집합의 표현, Time: 348 ms, Memory: 71112 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,42 +1,42 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**5)
 
 n, m = map(int, input().split())
-parent = [0] * (n+1)
+parent = []
 
 for i in range(n+1):
-    parent[i] = i
-
-def find(x):
-    if x == parent[x]:
-        return x
-    else:
-        parent[x] = find(parent[x])
-        return parent[x]
+    parent.append(i)
 
 def union(a, b):
     a = find(a)
     b = find(b)
-
+    
     if a != b:
         parent[b] = a
 
-def check(a, b):
+def find(a):
+    if a == parent[a]:
+        return a
+    else:
+        parent[a] = find(parent[a])
+        return parent[a]
+    
+def checksame(a, b):
     a = find(a)
     b = find(b)
 
     if a == b:
         return True
-    return False
+    else:
+        return False
+    
+for _ in range(m):
+    q, a, b = map(int, input().split())
 
-for i in range(m):
-    question, a, b = map(int, input().split())
-
-    if question == 0:
+    if q == 0:
         union(a, b)
     else:
-        if check(a, b):
+        if checksame(a, b):
             print("YES")
         else:
             print("NO")
