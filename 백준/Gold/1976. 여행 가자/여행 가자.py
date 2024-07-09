1711d9373901a866a31fcb75710886c4b9b64f12
Ornni <106477266+ornni@users.noreply.github.com>
2024-07-09 09:11:32 +0900

    [Gold IV] Title: 여행 가자, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -3,8 +3,12 @@ input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
+city = [[0 for _ in range(n+1)] for _ in range(n+1)]
 
-city= [[0 for j in range(n+1)] for i in range(n+1)]
+parent = [0] * (n+1)
+
+for i in range(1, n+1):
+    parent[i] = i
 
 for i in range(1, n+1):
     city[i] = list(map(int, input().split()))
@@ -12,10 +16,6 @@ for i in range(1, n+1):
 
 route = list(map(int, input().split()))
 route.insert(0, 0)
-
-parent = []
-for i in range(n+1):
-    parent.append(i)
 
 def find(a):
     if a == parent[a]:
@@ -27,6 +27,7 @@ def find(a):
 def union(a, b):
     a = find(a)
     b = find(b)
+
     if a != b:
         parent[b] = a
 
@@ -36,14 +37,14 @@ for i in range(1, n+1):
             union(i, j)
 
 index = find(route[1])
-isconnected = True
+isConnected = True
 
 for i in range(2, len(route)):
     if index != find(route[i]):
-        isconnected = False
+        isConnected = False
         break
 
-if isconnected:
+if isConnected:
     print("YES")
 else:
     print("NO")
