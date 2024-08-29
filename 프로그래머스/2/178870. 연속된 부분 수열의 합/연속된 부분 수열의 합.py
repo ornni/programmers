a4a06e0936ca5a2d4c5a0c9b60c21214d045beb7
Ornni <106477266+ornni@users.noreply.github.com>
2024-08-29 10:27:52 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 160.93 ms, Memory: 51.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -0,0 +1,15 @@
+def solution(sequence, k):
+    num = 0
+    
+    for i in range(len(sequence)-1, -1, -1):
+        num += sequence[i]
+        
+        if num > k:
+            num -= sequence.pop()
+        
+        if num == k:
+            while sequence[i-1] == sequence[-1] and i > 0:
+                i -= 1
+                sequence.pop()
+        
+            return [i, len(sequence)-1]
